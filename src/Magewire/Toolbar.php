f778d945cffe7fa3cd42dce48f033a411d8bd5c5
mvenghaus <marcus.venghaus@inklammern.de>
2023-08-08 07:11:31 +0200

    reset step to shipping on toolbar action

--- src/Magewire/Toolbar.php
+++ src/Magewire/Toolbar.php
@@ -6,16 +6,20 @@ namespace Hyva\AmazonPay\Magewire;
 
 use Magewirephp\Magewire\Component;
 use Hyva\AmazonPay\Model\AmazonPayCheckout;
+use Hyva\Checkout\Model\Session as SessionCheckoutConfig;
 
 class Toolbar extends Component
 {
     public function __construct(
+        private readonly SessionCheckoutConfig $sessionCheckoutConfig,
         private readonly AmazonPayCheckout $amazonPayCheckout
     ) {
     }
 
     public function backToStandard(): void
     {
+        $this->sessionCheckoutConfig->setCurrentStep('shipping');
+
         $this->amazonPayCheckout->deactivateCheckout();
 
         $this->redirect('checkout');
@@ -23,6 +27,8 @@ class Toolbar extends Component
 
     public function changeAmazonInformation(): void
     {
+        $this->sessionCheckoutConfig->setCurrentStep('shipping');
+
         $this->redirect($this->amazonPayCheckout->getChangeUrl());
     }
 }
